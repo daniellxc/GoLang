44ca068e2dd85aa2a18d9111dbdeff09e0f14ca7
Daniel Xavier Cardoso <daniel.cardoso@webmodulos.com.br>
2017-12-04 11:53:12 -0300

    Iniciando estudo com variáveis.

--- helo.go
+++ helo.go
@@ -2,8 +2,6 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Olá" + " Go Lang!")
-
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3.0=", 7.0/3.0)
+	var a string = "Inicial"
+	fmt.Println(a)
 }
